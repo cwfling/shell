3c9d78cb5abbe795ed43cd2423cd81b7ce2d9ede
CL <christopherwfling@gmail.com>
2025-10-14 00:45:58 -0400

    swapped to work in assignment branch instead of main to get the right pull request

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -53,7 +53,7 @@ cp *server*.log ../processed/server_logs/
 
 # 6. Repeat the above step for user logs and event logs
 
-cp *user*.log ../processed/user_logs/
+cp *user*.log ../processed/user_logs/git
 cp *event*.log ../processed/event_logs/
 
 
@@ -69,4 +69,3 @@ ls processed/*/* > inventory.txt
 ###########################################
 
 echo "Project setup is complete!"
- 
